7f864fba69b239d563b19061666e1aa09d8645f0
afonsojones <jjrcarvalho22@gmail.com>
2021-08-17 19:24:43 -0300

    ciclo2_desafio1

--- desafio5.js
+++ desafio5.js
@@ -6,7 +6,7 @@ document.write('1. Seja bem vindo '+nome+'!'+'<br>');
 
 document.write('2. Você digitou o número ('+numero+').'+'<br>');
 
-var v = (numero == 20);
+var v = (numero == numero);
 
 console.log(v);
 
@@ -14,23 +14,21 @@ document.write('3. O retorno da comparação booleana é '+v+'.'+'<br>');
 
 numero = parseInt(numero);
 
-var vinte = 20;
+console.log(numero+numero);
 
-console.log(numero+vinte);
-
-var soma = numero+vinte;
+var soma = numero+numero;
 
 document.write('4. A soma dos valores é '+soma+'.'+'<br>');
 
-console.log(numero-vinte);
+console.log(numero-numero);
 
-var sub = numero-vinte;
+var sub = numero-numero;
 
 document.write('5. A subtração dos valores é '+sub+'.'+'<br>');
 
-console.log(numero%vinte);
+console.log(numero%numero);
 
-var resto = numero%vinte;
+var resto = numero%numero;
 
 document.write('6. O resto da divisão é de '+resto+'.'+'<br>');
 
